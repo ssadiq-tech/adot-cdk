4ddf4502bf33757437795a89bb441d564cf6ec6e
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-22 18:16:05 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
-#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AdotEksCdkStack } from './adot-eks-cdk-stack';
+import { AdotEKSStack } from './adot-eks-cdk-stack';
 
 const app = new cdk.App();
-new AdotEksCdkStack(app, 'AdotEksStack');
+new AdotEKSStack(app, 'AdotEKSStack');

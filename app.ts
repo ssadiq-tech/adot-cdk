4f71dd30a792adc4c27690beeaf2e1c283b5d536
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-22 16:50:27 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AdotEksCdkStack } from '../lib/adot-eks-cdk-stack';
+import { AdotEksCdkStack } from './adot-eks-cdk-stack';
 
 const app = new cdk.App();
-new AdotEksCdkStack(app, 'AdotEksCdkStack', {
-  env: {
-    account: '131332286832',
-    region: 'us-east-1',
-  },
-});
+new AdotEksCdkStack(app, 'AdotEksStack');
